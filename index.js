8b8b2bbbc8669270f13241f00218325c2592badd
Luis Vazquez <lv120189@gmail.com>
2020-07-26 12:13:01 -0300

    server with node

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ res.setHeader('content-type', 'text/plain');
 res.end('Hello World made by Luis');
 });
 
-server.listen(3000, () => {
-    console.log('server is running on port 3000');
+server.listen(3030, () => {
+    console.log('server is running on port 3030');
 });
