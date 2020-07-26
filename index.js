fb32bce8aca9e06f5ce293ea9dd9c05dede4f788
Luis Vazquez <lv120189@gmail.com>
2020-07-26 17:58:53 -0300

    middleware

--- index.js
+++ index.js
@@ -11,21 +11,48 @@ server.listen(3030, () => {
 }); */
 
 const express = require('express');
-
 const app = express();
+
+function logger(req, res, next){
+console.log(`Route received: ${ req.protocol }://${req.get('host')}${req.originalUrl} `);
+next();
+}
+
+//---MIDDLEWARE---
+app.use(express.json());
+app.use(logger);
+//-----------------------
+
+
 app.get('/', (req, res) => {
-    res.send('PETICION GET RECIBIDA')
+    res.send('hola mundo')
+});
+app.get('/user', (req, res) => {
+    res.json({
+        name: 'Luis',
+        lastName: 'Vazquez',
+        age: 31,
+        user: 'sugus'
+    })
 });
 
-app.post('/about', (req, res) => {
+app.post('/user', (req, res) => {
+    console.log(req.body);
     res.send('REQUEST POST RECIBIDA')
 });
-app.put('/contact', (req, res) => {
-    res.send('REQUEST PUT RECIBIDA')
+
+app.post('/user/:id', (req, res) => {
+    console.log(req.params);
+    res.send('REQUEST POST RECIBIDA')
 });
 
-app.delete('/contact', (req, res) => {
-    res.send('REQUEST DELETE RECIBIDA')
+app.put('/user/:id', (req, res) => {
+    console.log(req.body);
+    res.send(`the user ${req.params.id} updated`)
+});
+
+app.delete('/user/:id', (req, res) => {
+    res.send(`user ${req.params.id} deleted`)
 });
 
 app.listen(3000, () => {
