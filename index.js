4496ccc6660ee2ee18f720c04b33df5e8abb0358
Luis Vazquez <lv120189@gmail.com>
2020-07-26 16:27:43 -0300

    utilizando express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 
 const server =  http.createServer((req, res) => {
 res.status = 200;
@@ -8,4 +8,27 @@ res.end('Hello World made by Luis');
 
 server.listen(3030, () => {
     console.log('server is running on port 3030');
+}); */
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('PETICION GET RECIBIDA')
 });
+
+app.post('/about', (req, res) => {
+    res.send('REQUEST POST RECIBIDA')
+});
+app.put('/contact', (req, res) => {
+    res.send('REQUEST PUT RECIBIDA')
+});
+
+app.delete('/contact', (req, res) => {
+    res.send('REQUEST DELETE RECIBIDA')
+});
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
+
